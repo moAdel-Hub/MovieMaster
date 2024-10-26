dbf1c8cab759afe8aba5f35e00934c7e61dd5c04
moAdel-Hub <mohomed.adel.26@gmail.com>
2024-10-26 13:27:19 +0300

    Initial Commit

---
+++ MovieMaster/MovieMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMasterApp.swift
+//  MovieMaster
+//
+//  Created by Mohamed Ali on 26/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
